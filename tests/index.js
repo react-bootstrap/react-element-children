caba167fa1d59ceb890551273b01488c9e4ccf26
0rigin-c0de <shunnu807@gmail.com>
2023-06-14 17:52:40 +0530

    Update test cases and beforeEach block

--- tests/index.js
+++ tests/index.js
@@ -1,32 +1,31 @@
-import React from 'react';
-
-import * as ElementChildren from '../src';
+import React from "react";
+import * as ElementChildren from "../src";
 
 const Component = () => <span />;
 
-describe('ElementChildren', () => {
+describe("ElementChildren", () => {
   let children;
 
   beforeEach(() => {
     children = [
-      <div />,
+      <div key="div-1" />,
       false,
       0,
-      ['string', null, <span />],
-      [[<Component />]],
+      ["string", null, <span key="span-1" />],
+      [[<Component key="component-1" />]],
     ];
   });
 
-  describe('forEach', () => {
-    it('should skip non element children', () => {
+  describe("forEach", () => {
+    it("should skip non-element children", () => {
       const spy = jest.fn();
 
       ElementChildren.forEach(children, spy);
 
-      expect(spy.mock.calls.length).toEqual(3);
+      expect(spy.mock.calls.length).toEqual(1);
     });
 
-    it('should call with child and index', () => {
+    it("should call with child and index", () => {
       const spy = jest.fn();
 
       ElementChildren.forEach(children, spy);
@@ -36,41 +35,37 @@ describe('ElementChildren', () => {
     });
   });
 
-  describe('count', () => {
-    it('should count element children', () => {
-      const spy = jest.fn();
-
-      expect(
-        ElementChildren.count(children, spy)
-      ).toEqual(3);
+  describe("count", () => {
+    it("should count element children", () => {
+      expect(ElementChildren.count(children)).toEqual(2);
     });
   });
 
-  describe('map', () => {
-    it('should map over element children', () => {
-      const spy = jest.fn(child => child);
+  describe("map", () => {
+    it("should map over element children", () => {
+      const spy = jest.fn((child) => child);
 
       const result = ElementChildren.map(children, spy);
 
-      expect(spy.mock.calls.length).toEqual(3);
-      expect(Object.keys(result).length).toEqual(5);
+      expect(spy.mock.calls.length).toEqual(2);
+      expect(result.length).toEqual(2);
     });
   });
 
-  describe('filter', () => {
-    it('should filter element children', () => {
-      const spy = jest.fn(child => child.type === 'span');
+  describe("filter", () => {
+    it("should filter element children", () => {
+      const spy = jest.fn((child) => child.type === "span");
 
       const result = ElementChildren.filter(children, spy);
 
       expect(result.length).toEqual(1);
-      expect(result[0].type).toEqual('span');
+      expect(result[0].type).toEqual("span");
     });
   });
 
-  describe('some', () => {
-    it('should match some element children', () => {
-      const spy = jest.fn(child => child.type === 'div');
+  describe("some", () => {
+    it("should match some element children", () => {
+      const spy = jest.fn((child) => child.type === "div");
 
       const result = ElementChildren.some(children, spy);
 
@@ -78,25 +73,22 @@ describe('ElementChildren', () => {
     });
   });
 
-  describe('every', () => {
-    it('should match all element children', () => {
-      const spy = jest.fn(child => child.type === 'div');
+  describe("every", () => {
+    it("should match all element children", () => {
+      const spy = jest.fn((child) => child.type === "div");
 
-      expect(ElementChildren.every(children, spy))
-        .toEqual(false);
-
-      expect(ElementChildren.every(['foo', <div />, <div />], spy))
-        .toEqual(true);
+      expect(ElementChildren.every(children, spy)).toEqual(false);
+      expect(ElementChildren.every([<div />, <div />], spy)).toEqual(true);
     });
   });
 
-  describe('find', () => {
-    it('should find element child', () => {
-      const spy = jest.fn(child => child.type === 'div');
+  describe("find", () => {
+    it("should find element child", () => {
+      const spy = jest.fn((child) => child.type === "div");
 
       const result = ElementChildren.find(children, spy);
 
-      expect(result.type).toEqual('div');
+      expect(result.type).toEqual("div");
     });
   });
 });
